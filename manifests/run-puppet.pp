46386ede562cdaa00187e181508c2425c3e21506
msamuels <mijujoel@acm.org>
2018-03-28 02:10:44 +0000

    run-puppet.pp

--- manifests/run-puppet.pp
+++ manifests/run-puppet.pp
@@ -1,3 +1,9 @@
+# Change to puppet manifest
+file { '/tmp/hello.txt':
+  ensure  => file,
+  content => "#This file is managed by Puppet - manual edits will be lost\n\nhello, world\n",
+}
+
 # Set up regular Puppet runs
 file { '/usr/local/bin/run-puppet':
   source => '/etc/puppetlabs/code/environments/production/files/run-puppet.sh',
